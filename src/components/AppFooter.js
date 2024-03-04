d92e7843dde46e3aeca0b8f696e353c99b1c0795
muskan2622 <singhmuskan2603@gmail.com>
2024-03-05 04:24:23 +0530

    Data Visulation Dashboard

---
+++ src/components/AppFooter.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { CFooter } from '@coreui/react'
+
+const AppFooter = () => {
+  return (
+    <CFooter className="px-4">
+      <div>
+        <a target="_blank" rel="noopener noreferrer">
+          Made By Muskan for Black coffer
+        </a>
+        <span className="ms-1"></span>
+      </div>
+      <div className="ms-auto">
+        <span className="me-1"></span>
+      </div>
+    </CFooter>
+  )
+}
+
+export default React.memo(AppFooter)
